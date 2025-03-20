c360ca94b504556c856dcba9d1572835516b03e4
Divya Bharatbhai Chaudhary <divya-bharatbhai.chaudhary@epita.fr>
2025-03-20 12:05:06 +0100

    9th

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Import the route
 const arithmeticRoute = require('./routes/arithmeticRoute');
